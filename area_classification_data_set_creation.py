81acf6e9f90a2d1dfce61777bbc9f17101b2a7ee
MNasilowski <nasilowski.marcin@gmail.com>
2021-02-10 14:32:07 +0100

    changes

--- area_classification_data_set_creation.py
+++ area_classification_data_set_creation.py
@@ -2,6 +2,12 @@ import cv2
 import os
 import numpy as np
 import pandas as pd
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+x_size = int(config['main']['x_size'])
+y_size = int(config['main']['y_size'])
+classes_names = list(config['classes'].values())
 
 files = os.listdir('Data')
 columns_names = []
@@ -9,7 +15,7 @@ x_size, y_size = 1830,1830
 X = np.zeros((x_size,y_size,len(files)))
 for i, file in enumerate(files):
     columns_names.append(file[-7:-4])
-    X[...,i] = cv2.imread(os.path.join('Data', file), cv2.IMREAD_UNCHANGED)
+    X[...,i] = cv2.imread(os.path.join('Data', file), cv2.IMREAD_UNCHANGED)[:x_size,:y_size]
 data = X.reshape((x_size*y_size, len(files)))
 
 for i in range(data.shape[1]):
@@ -20,12 +26,12 @@ from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range=(0, 1))
 data = sc.fit_transform(data)
 
-classes = cv2.imread('Classification.png')[:x_size, :y_size]
+classes = cv2.imread('data/classes/Classification.png')[:x_size, :y_size]
 classes = classes.reshape((x_size*y_size, 3))
 classes = (classes/255).astype(int)
 
 other = (1 - classes.any(axis=1).astype(int)).reshape(-1,1)
-columns_names += ['water', 'forest', 'fields', 'other']
+columns_names += classes_names
 
 data = np.concatenate((data, classes, other), axis=1)
 data = pd.DataFrame(data, columns=columns_names)
--- settings.py
+++ settings.py
@@ -6,22 +6,20 @@ Created on Fri Feb  5 11:48:27 2021
 """
 
 from PIL import Image 
+x_size, y_size = Image.open("Data/Images/T34UDE_20200815T095039_B01.png").size
 
-
-
-
-
-
-
+# I realy don't nead this file...
 from configparser import ConfigParser
 config = ConfigParser()
-
-config.read('config.ini')
 config.add_section('main')
-config.set('main', 'key1', 'value1')
-config.set('main', 'key2', 'value2')
-config.set('main', 'key3', 'value3')
+config.set('main', 'x_size', str(x_size))
+config.set('main', 'y_size', str(y_size))
 
+config.add_section('classes')
+config.set('classes', 'class_1', 'water')
+config.set('classes', 'class_2', 'forest')
+config.set('classes', 'class_3', 'fields')
+config.set('classes', 'class_4', 'other')
 
 with open('config.ini', 'w') as f:
     config.write(f)
