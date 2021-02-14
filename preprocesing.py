6cee6585c6f301e46878b91bc744cfb178c5a4ca
mnasilowski <nasilowski.marcin@gmail.com>
2021-02-14 12:25:29 +0100

    changes

--- preprocesing.py
+++ preprocesing.py
@@ -6,15 +6,15 @@ from configparser import ConfigParser
 from sklearn.preprocessing import MinMaxScaler
 
 
-def images_to_numpy(input_dir,size):
+def images_to_numpy(input_dir, dx, dy, x=0, y=0):
     files = os.listdir(input_dir)
     columns_names = []
-    X = np.zeros((size[0], size[1], len(files)))
+    X = np.zeros((dx, dy, len(files)))
     for i, file in enumerate(files):
         file_path = os.path.join(input_dir, file)
-        X[...,i] = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
+        X[...,i] = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)[x:x+dx,y:y+dy]
         columns_names.append(file[-11:-8])
-    data = X.reshape((size[0]*size[1], len(files)))
+    data = X.reshape((dx*dy, len(files)))
     return data, columns_names
 
 def remove_outstandings(data):
@@ -25,13 +25,15 @@ def remove_outstandings(data):
     data = sc.fit_transform(data)
     return data
 
-def get_classes(class_dir, size):
+def get_classes(class_dir, dx, dy, x=0, y=0):
+    
     files = os.listdir(class_dir)
-    X = np.zeros((size[0], size[1], len(files)))
+    X = np.zeros((dx, dy, len(files)))
     for i, file in enumerate(files):
         file_path = os.path.join(class_dir, file)
-        X[...,i] = np.rint(cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)/255)
-    classes = X.reshape((size[0]*size[1], len(files)))
+        img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)[x:x+dx,y:y+dy]
+        X[...,i] = np.rint(img/255)
+    classes = X.reshape((dx*dy, len(files)))
     return classes
 
 if __name__ == '__main__':
@@ -39,16 +41,19 @@ if __name__ == '__main__':
     config.read('config.ini')
     input_dir = config['main']['input_dir']
     class_file = config['main']['classification_data']
-    x_size = int(config['main']['x_size'])
-    y_size = int(config['main']['y_size'])
+    dx = int(config['main']['x_size'])
+    dy = int(config['main']['y_size'])
+    x = int(config['main']['x_start'])
+    y = int(config['main']['y_start'])
     class_names = list(config['classes'].values())
     csv_data_file = config['main']['csv_data_file']
 
-    data, columns_names = images_to_numpy(input_dir, (x_size,y_size))
+    data, columns_names = images_to_numpy(input_dir, dx, dy, x, y)
     data = remove_outstandings(data)
-    classes = get_classes(class_file, (x_size,y_size))
+    classes = get_classes(class_file, dx, dy, x, y)
     other = (1 - classes.any(axis=1).astype(int)).reshape(-1,1)
     columns_names += class_names 
     data = np.concatenate((data, classes, other), axis=1)
     data = pd.DataFrame(data, columns=columns_names)
     data.to_csv(csv_data_file)
+
--- settings.py
+++ settings.py
@@ -5,31 +5,24 @@ Created on Fri Feb  5 11:48:27 2021
 @author: nasil
 """
 
-from PIL import Image
-import os
 
-def get_size(input_dir):
-    """ Check resolution of image in input dir
-    ToDo: assert that all images have the same resolution
-    """
-    file = os.listdir(input_dir)[0]
-    x, y = Image.open(os.path.join(input_dir,file)).size[:2]
-    return x, y
 
 from configparser import ConfigParser
 config = ConfigParser()
 config.add_section('main')
 config.set('main', 'input_dir', 'Data/R60m_png')
 config.set('main', 'classification_data', 'Data/Classes')
-x_size, y_size = get_size(config['main']['input_dir'])
-config.set('main', 'x_size', str(x_size))
-config.set('main', 'y_size', str(y_size))
+config.set('main', 'x_size', '750')
+config.set('main', 'y_size', '750')
+config.set('main', 'x_start', '250')
+config.set('main', 'y_start', '250')
 config.set('main', 'csv_data_file', 'data_set.csv')
 config.add_section('classes')
 config.set('classes', 'class_1', 'water')
 config.set('classes', 'class_2', 'forest')
 config.set('classes', 'class_3', 'fields')
 config.set('classes', 'class_4', 'other')
+
 
 with open('config.ini', 'w') as f:
     config.write(f)
