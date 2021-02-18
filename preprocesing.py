e1533ea35f508827798db5874f1ec1f6130c8845
mnasilowski <nasilowski.marcin@gmail.com>
2021-02-18 10:40:55 +0100

    corrected classes names

--- preprocesing.py
+++ preprocesing.py
@@ -8,7 +8,6 @@ from sklearn.preprocessing import MinMaxScaler
 
 def images_to_numpy(input_dir, dx, dy, x=0, y=0):
     files = os.listdir(input_dir)
-    print("diles: ", files)
     columns_names = []
     X = np.zeros((dx, dy, len(files)))
     for i, file in enumerate(files):
@@ -27,17 +26,26 @@ def remove_outstandings(data):
     return data
 
 def get_classes(class_dir, dx, dy, x=0, y=0):
-    
     files = os.listdir(class_dir)
-    print("diles: ", files)
+    columns_names = []
     X = np.zeros((dx, dy, len(files)))
     for i, file in enumerate(files):
+        columns_names.append(file[:-4])
         file_path = os.path.join(class_dir, file)
         img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)[x:x+dx,y:y+dy]
         X[...,i] = np.rint(img/255)
     classes = X.reshape((dx*dy, len(files)))
-    return classes
+    return classes, columns_names
 
+def add_classes_to_config(config, classes):
+    config.remove_section('classes')
+    config.add_section('classes')
+    for i, name in enumerate(classes):
+        config.set('classes', f'class_{i}', name)
+    with open('config.ini', 'w') as f:
+        config.write(f)
+    return config 
+    
 if __name__ == '__main__':
     config = ConfigParser()
     config.read('config.ini')
@@ -47,15 +55,15 @@ if __name__ == '__main__':
     dy = int(config['main']['y_size'])
     x = int(config['main']['x_start'])
     y = int(config['main']['y_start'])
-    class_names = list(config['classes'].values())
     csv_data_file = config['main']['csv_data_file']
 
     data, columns_names = images_to_numpy(input_dir, dx, dy, x, y)
     data = remove_outstandings(data)
-    classes = get_classes(class_file, dx, dy, x, y)
+    classes, class_names = get_classes(class_file, dx, dy, x, y)
     other = (1 - classes.any(axis=1).astype(int)).reshape(-1,1)
+    class_names += ['other']
+    add_classes_to_config(config, class_names)
     columns_names += class_names 
     data = np.concatenate((data, classes, other), axis=1)
     data = pd.DataFrame(data, columns=columns_names)
     data.to_csv(csv_data_file)
-
--- settings.py
+++ settings.py
@@ -5,26 +5,25 @@ Created on Fri Feb  5 11:48:27 2021
 @author: nasil
 """
 
-
-
 from configparser import ConfigParser
-config = ConfigParser()
-config.add_section('main')
-config.set('main', 'input_dir', 'Data/R10m_png')
-config.set('main', 'classification_data', 'Data/C10m')
-config.set('main', 'x_size', '1000')
-config.set('main', 'y_size', '1000')
-config.set('main', 'x_start', '3200')
-config.set('main', 'y_start', '250')
-config.set('main', 'csv_data_file', 'data_set.csv')
-config.add_section('classes')
-config.set('classes', 'class_1', 'water')
-config.set('classes', 'class_2', 'forest')
-config.set('classes', 'class_3', 'fields')
-config.set('classes', 'class_4', 'other')
-
-
-with open('config.ini', 'w') as f:
-    config.write(f)
     
     
+def r10m_small():
+    config = ConfigParser()
+    config.add_section('main')
+    config.set('main', 'input_dir', 'Data/R10m_png')
+    config.set('main', 'classification_data', 'Data/C10m')
+    config.set('main', 'x_size', '10')
+    config.set('main', 'y_size', '10')
+    config.set('main', 'x_start', '3200')
+    config.set('main', 'y_start', '250')
+    config.set('main', 'csv_data_file', 'data_set.csv')
+    config.add_section('classes')
+    config.set('classes', 'class_1', 'water')
+    config.set('classes', 'class_2', 'forest')
+    config.set('classes', 'class_3', 'fields')
+    config.set('classes', 'class_4', 'other')
+    with open('config.ini', 'w') as f:
+        config.write(f)
+    
+r10m_small()
